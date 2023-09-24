cbe99597368263ca136cb4e5ac8deba2139c1765
Lawal Tajudeen <lwltjdn@gmail.com>
2023-09-24 16:54:54 +0100

    polster

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Questions, Choice
+
+admin.site.site_header = 'The CybersForth Academy'
+admin.site.site_title = 'The CybersForth polls'
+admin.site.index_title = 'Welcome to the CybersForth Poll admin area'
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+              ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInline]
+
+admin.site.register(Questions, QuestionAdmin)
